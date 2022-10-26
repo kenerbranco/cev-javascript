f7a2b42f27b95fb9727dfd608a423e33cc65987d
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-10-26 18:11:57 -0300

    finalizei ex014

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,19 +1,21 @@
-var agora = new Date()
-var horario = agora.getHours()
-
-txtHora = window.document.getElementById('hora')
-imagem = window.document.getElementById('img')
-corpo = window.document.getElementById('corpo')
-
-txtHora.innerHTML = `Agora são ${horario} horas`
-
-if (horario >= 18 && horario < 24) {
-    imagem.innerHTML = `<img src="imagens/noite.jpg" alt="foto noite">`
-    corpo.style.background = 'rgb(66, 66, 87)'
-} else if (horario >= 0 && horario < 12) {
-    imagem.innerHTML = `<img src="imagens/manha.jpg" alt="foto manha">`
-    corpo.style.background = 'rgb(230, 231, 126)'
-} else {
-    imagem.innerHTML = `<img src="imagens/tarde.jpg" alt="foto tarde">`
-    corpo.style.background = 'rgb(241, 147, 85)'
+function carregar() {
+    var agora = new Date()
+    var horario = agora.getHours()
+    
+    txtHora = window.document.getElementById('hora')
+    imagem = window.document.getElementById('img')
+    corpo = window.document.getElementById('corpo')
+    
+    txtHora.innerHTML = `Agora são ${horario} horas`
+    
+    if (horario >= 18 && horario < 24) {
+        imagem.innerHTML = `<img src="imagens/noite.jpg" alt="foto noite">`
+        document.body.style.background = 'rgb(66, 66, 87)'
+    } else if (horario >= 0 && horario < 12) {
+        imagem.innerHTML = `<img src="imagens/manha.jpg" alt="foto manha">`
+        document.body.style.background = 'rgb(230, 231, 126)'
+    } else {
+        imagem.innerHTML = `<img src="imagens/tarde.jpg" alt="foto tarde">`
+        document.body.style.background = 'rgb(241, 147, 85)'
+    }
 }
